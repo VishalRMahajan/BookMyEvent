afc63c00aa58cc80ac8f1279e6c070e6c679c9cf
Vishal Mahajan <111660265+VishalRMahajan@users.noreply.github.com>
2024-02-06 02:02:43 +0530

    Update views.py

--- Backend/modules/email/views.py
+++ Backend/modules/email/views.py
@@ -7,7 +7,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'wraptalentauthentication@gmail.com'  # replace with your email
 
-app.config['MAIL_PASSWORD'] =  'dbas rlyb avua eyks'
+app.config['MAIL_PASSWORD'] =  ''
 mail = Mail(app)
 
 
