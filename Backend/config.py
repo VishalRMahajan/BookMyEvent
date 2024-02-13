5fe34e061e4d40694360fc0830b1569969a770c9
Vishal Mahajan <vism06@gmail.com>
2024-02-14 00:49:19 +0530

    Initial

--- Backend/config.py
+++ Backend/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://sfitevents_user:dTFOHB7tKXU3ZcjrI2NO3stCm7xczpU9@dpg-cmt9qkocmk4c738m42p0-a.oregon-postgres.render.com/sfitevents'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../databases/database.db'
     SECRET_KEY = 'secretkey'
