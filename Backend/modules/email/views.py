1142bc3746812d493c8be5fead33c1728518e775
Vishal Mahajan <vism06@gmail.com>
2024-04-09 18:18:05 +0530

    Removed Unwanted Files and Data

--- Backend/modules/email/views.py
+++ Backend/modules/email/views.py
@@ -5,14 +5,14 @@ import os
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'wraptalentauthentication@gmail.com'  # replace with your email
+app.config['MAIL_USERNAME'] = ''
 
 app.config['MAIL_PASSWORD'] =  ''
 mail = Mail(app)
 
 
 def send_otp(email, otp):
-    msg = Message('OTP for your account', sender='wraptalentauthentication@gmail.com', recipients=[email])
+    msg = Message('OTP for your account', sender='', recipients=[email])
     msg.body = f'Your OTP is {otp}'
     
     mail.send(msg)
